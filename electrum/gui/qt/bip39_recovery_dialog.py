ae4b50115b26f5e473cdf06594a1ec37bf27080a
Luke Childs <lukechilds123@gmail.com>
2020-06-14 14:10:51 +0700

    Make sure we always get the correct window

--- electrum/gui/qt/bip39_recovery_dialog.py
+++ electrum/gui/qt/bip39_recovery_dialog.py
@@ -3,10 +3,13 @@
 # file LICENCE or http://www.opensource.org/licenses/mit-license.php
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QGridLayout, QLabel
 from electrum.i18n import _
-from .util import WindowModalDialog, Buttons, CancelButton, OkButton
+from .util import WindowModalDialog, MessageBoxMixin, Buttons, CancelButton, OkButton
 
 class Bip39RecoveryDialog(WindowModalDialog):
     def __init__(self, parent: QWidget):
+        assert parent
+        if isinstance(parent, MessageBoxMixin):
+            parent = parent.top_level_window()
         WindowModalDialog.__init__(self, parent, _('BIP39 Recovery'))
         self.setMinimumWidth(400)
         vbox = QVBoxLayout(self)
