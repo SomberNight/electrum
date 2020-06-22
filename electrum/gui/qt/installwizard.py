ee1c2cd14d3ca129fa28b398498d94a798a244ec
Luke Childs <lukechilds123@gmail.com>
2020-06-22 15:27:56 +0700

    Only allow BIP39 recovery when we have a BIP39 seed

--- electrum/gui/qt/installwizard.py
+++ electrum/gui/qt/installwizard.py
@@ -609,21 +609,19 @@ class InstallWizard(QDialog, MessageBoxMixin, BaseWizard):
                                run_next, default_choice_idx: int=0) -> Tuple[str, str]:
         vbox = QVBoxLayout()
 
-        # TODO: Test with HWW import
-        # It'll probably break since we don't have access to the seed
-        # but we can probably fix by instead using the xpub.
-        button = QPushButton(_("Detect Existing Accounts"))
-        def on_account_select(account):
-            script_type = account["script_type"]
-            if script_type == "p2pkh":
-                script_type = "standard"
-            button_index = c_values.index(script_type)
-            button = clayout.group.buttons()[button_index]
-            button.setChecked(True)
-            line.setText(account["derivation_path"])
-        button.clicked.connect(lambda: Bip39RecoveryDialog(self, self.seed, self.passphrase, on_account_select))
-        vbox.addWidget(button, alignment=Qt.AlignLeft)
-        vbox.addWidget(QLabel(_("Or")))
+        if self.seed_type == "bip39":
+            button = QPushButton(_("Detect Existing Accounts"))
+            def on_account_select(account):
+                script_type = account["script_type"]
+                if script_type == "p2pkh":
+                    script_type = "standard"
+                button_index = c_values.index(script_type)
+                button = clayout.group.buttons()[button_index]
+                button.setChecked(True)
+                line.setText(account["derivation_path"])
+            button.clicked.connect(lambda: Bip39RecoveryDialog(self, self.seed, self.passphrase, on_account_select))
+            vbox.addWidget(button, alignment=Qt.AlignLeft)
+            vbox.addWidget(QLabel(_("Or")))
 
         c_values = [x[0] for x in choices]
         c_titles = [x[1] for x in choices]
