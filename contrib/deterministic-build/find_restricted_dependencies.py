eb9db85fb8d314b0bd5e6fe8badb92a71f8b56a7
SomberNight <somber.night@protonmail.com>
2023-05-26 15:18:10 +0000

    contrib: add blacklist to find_restricted_dependencies.py
    we don't want "colorama" if it is only pulled in by "qrcode"
    - colorama is a PITA to build, see https://github.com/spesmilo/electrum/commit/590abec33934a2da41e47ecd9c5f48bdc758aae5
    - qrcode only needs it for CLI use and only on Windows. N/A for us

--- contrib/deterministic-build/find_restricted_dependencies.py
+++ contrib/deterministic-build/find_restricted_dependencies.py
@@ -7,14 +7,26 @@ except ImportError as e:
     sys.exit(f"Error: {str(e)}. Try 'sudo python3 -m pip install <module-name>'")
 
 
-def check_restriction(p, r):
+def is_blacklisted(pkg: str, *, pulled_in_by: str) -> bool:
+    if pkg == "colorama" and pulled_in_by == "qrcode":
+        # colorama 0.4.6+ requires hatchling to build, see https://github.com/spesmilo/electrum/commit/590abec33934a2da41e47ecd9c5f48bdc758aae5
+        # qrcode only needs colorama for CLI use, which we don't need
+        return True
+    return False
+
+
+def want_restricted_pkg(pkg: str, restriction: str, *, pulled_in_by: str) -> bool:
+    #print(f"xxxxx. want_restricted_pkg(). {pkg=}. {restriction=}. {pulled_in_by=}", file=sys.stderr)
+    if is_blacklisted(pkg, pulled_in_by=pulled_in_by):
+        return False
     # See: https://www.python.org/dev/peps/pep-0496/
     # Hopefully we don't need to parse the whole microlanguage
-    if "extra" in r and "[" not in p:
+    if "extra" in restriction and "[" not in pkg:
         return False
     for marker in ["os_name", "platform_release", "sys_platform", "platform_system"]:
-        if marker in r:
+        if marker in restriction:
             return True
+    return False
 
 
 for p in sys.stdin.read().split():
@@ -36,8 +48,8 @@ for p in sys.stdin.read().split():
             dep = dep.strip()
             restricted = restricted.strip()
             dep_basename = dep.split(" ")[0]
-            if check_restriction(dep, restricted):
-                print(dep_basename, sep=" ")
+            if want_restricted_pkg(dep, restricted, pulled_in_by=p):
+                print(dep_basename, sep=" ", file=sys.stdout)
                 print("Installing {} from {} although it is only needed for {}".format(dep, p, restricted), file=sys.stderr)
     except TypeError:
         # Has no dependencies at all
