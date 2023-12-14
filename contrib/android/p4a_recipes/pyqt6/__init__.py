9f350ecd08e3c3da917f9cba9c33567a9cb610a3
SomberNight <somber.night@protonmail.com>
2023-12-14 23:41:26 +0000

    tmp9

--- contrib/android/p4a_recipes/pyqt6/__init__.py
+++ contrib/android/p4a_recipes/pyqt6/__init__.py
@@ -6,13 +6,13 @@ from pythonforandroid.util import load_source
 util = load_source('util', os.path.join(os.path.dirname(os.path.dirname(__file__)), 'util.py'))
 
 
-assert PyQt6Recipe._version == "6.6.1"
+assert PyQt6Recipe._version == "6.6.0"
 assert PyQt6Recipe.depends == ['qt6', 'pyjnius', 'setuptools', 'pyqt6sip', 'hostpython3', 'pyqt_builder']
 assert PyQt6Recipe.python_depends == []
 
 
 class PyQt6RecipePinned(util.InheritedRecipeMixin, PyQt6Recipe):
-    sha512sum = "c2ff8c47c9b0e43c009d0c90f565a54344e6f384c67dd30c2d422465d0702c07713acc0095c8b67827d1146675611c07d548ba282a26e41bb60a0a21977a7a64"
+    sha512sum = "2fe8640b1dc82aa1da6064da2ef6c4ee81216ed34f28211b69d22c5ea00f782298f5a6a94d32ab00c3ee095abf15d2182a17324cd132458580f5659e789686e3"
 
 
 recipe = PyQt6RecipePinned()
