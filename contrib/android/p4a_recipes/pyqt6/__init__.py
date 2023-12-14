6694ab9701f8f3a991ed5fee400dc65551dd4ccf
SomberNight <somber.night@protonmail.com>
2023-12-14 16:13:01 +0000

    tmp14

--- contrib/android/p4a_recipes/pyqt6/__init__.py
+++ contrib/android/p4a_recipes/pyqt6/__init__.py
@@ -6,13 +6,13 @@ from pythonforandroid.util import load_source
 util = load_source('util', os.path.join(os.path.dirname(os.path.dirname(__file__)), 'util.py'))
 
 
-assert PyQt6Recipe._version == "6.4.2"
+assert PyQt6Recipe._version == "6.5.3"
 assert PyQt6Recipe.depends == ['qt6', 'pyjnius', 'setuptools', 'pyqt6sip', 'hostpython3', 'pyqt_builder']
 assert PyQt6Recipe.python_depends == []
 
 
 class PyQt6RecipePinned(util.InheritedRecipeMixin, PyQt6Recipe):
-    sha512sum = "51e5f0d028ee7984876da1653cb135d61e2c402f18b939a92477888cc7c86d3bc2889477403dee6b3d9f66519ee3236d344323493b4c2c2e658e1637b10e53bf"
+    sha512sum = "a502693cc9e1887011466132cd85e232ce653bfc38519aed99a77332413bdb65a01ecad4680b831eb76365b086e6a3b52fa69017b39d95933a6372d2e7e8e4bb"
 
 
 recipe = PyQt6RecipePinned()
--- contrib/android/p4a_recipes/pyqt_builder/__init__.py
+++ contrib/android/p4a_recipes/pyqt_builder/__init__.py
@@ -1,13 +1,13 @@
 from pythonforandroid.recipes.pyqt_builder import PyQtBuilderRecipe
 
 
-assert PyQtBuilderRecipe._version == "1.15.1"
+assert PyQtBuilderRecipe._version == "1.15.4"
 assert PyQtBuilderRecipe.depends == ["sip", "packaging", "python3"]
 assert PyQtBuilderRecipe.python_depends == []
 
 
 class PyQtBuilderRecipePinned(PyQtBuilderRecipe):
-    sha512sum = "61ee73b6bb922c04739da60025ab50d35d345d2e298943305fcbd3926cda31d732cc5e5b0dbfc39f5eb85c0f0b091b8c3f5fee00dcc240d7849c5c4191c1368a"
+    sha512sum = "ec0b9f7784a32af744111615b93f98d73f284bb752fd71359c798d3b093a01925823effea72c866a5f49f77e3dfc5dee4125bbb289f647d84000bf34b5db6931"
 
 
 recipe = PyQtBuilderRecipePinned()
